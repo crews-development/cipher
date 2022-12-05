7d67484a8af6f72165ca8e79753e5f63d655ff5d
Tommy Crews <59206655+twcrews@users.noreply.github.com>
2022-12-04 18:19:18 -0600

    Added first combination selectors.

---
+++ src/Crews.Web.Cipher/Css/AdjacentSiblingSelector.cs
@@ -0,0 +1,11 @@
+﻿namespace Crews.Web.Cipher.Css;
+
+/// <summary>
+/// A CSS selector combination that uses the adjacent sibling combinator (+).
+/// </summary>
+public class AdjacentSiblingSelector : SelectorCombination
+{
+	/// <inheritdoc cref="SelectorCombination(char, Selector[])" />
+	public AdjacentSiblingSelector(params Selector[] baseSelectors)
+		: base('+', baseSelectors) { }
+}
