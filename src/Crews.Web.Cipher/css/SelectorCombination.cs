4583d1ae85b955d7b9c310bbcbe11cb701c036ec
Tommy Crews <59206655+twcrews@users.noreply.github.com>
2022-12-04 18:19:03 -0600

    Added first combination selectors.

---
+++ src/Crews.Web.Cipher/css/SelectorCombination.cs
@@ -0,0 +1,18 @@
+﻿namespace Crews.Web.Cipher.Css;
+
+/// <summary>
+/// Represents a CSS combinator selector.
+/// </summary>
+public abstract class SelectorCombination : Selector
+{
+	/// <summary>
+	/// Combinator constructor.
+	/// </summary>
+	/// <param name="combinator">The combinator separator.</param>
+	/// <param name="baseSelectors">The selectors to combine.</param>
+	public SelectorCombination(char combinator, params Selector[] baseSelectors)
+		: base(GetBaseSelector(combinator, baseSelectors)) { }
+
+	private static string GetBaseSelector(char combinator, Selector[] selectors) =>
+		string.Join(combinator, selectors.Select(s => s.ToString()));
+}
