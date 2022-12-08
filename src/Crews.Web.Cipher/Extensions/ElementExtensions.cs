9fc60ece7000f55cecc4e75f906ee098cf385725
Tommy Crews <59206655+twcrews@users.noreply.github.com>
2022-12-07 22:48:56 -0600

    Added extension methods.

---
+++ src/Crews.Web.Cipher/Extensions/ElementExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+using Crews.Web.Cipher.Html;
+using Microsoft.AspNetCore.Html;
+
+namespace Crews.Web.Cipher.Extensions;
+
+public static class ElementExtensions
+{
+	public static HtmlString ToHtmlString(this Element element) =>
+		new HtmlString(element.ToString());
+}
