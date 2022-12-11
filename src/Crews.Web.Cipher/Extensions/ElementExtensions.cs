816dc52d92f7c57133210d7bfe6dfcbc35cbe075
Tommy Crews <59206655+twcrews@users.noreply.github.com>
2022-12-10 19:54:26 -0600

    Added documentation.

--- src/Crews.Web.Cipher/Extensions/ElementExtensions.cs
+++ src/Crews.Web.Cipher/Extensions/ElementExtensions.cs
@@ -4,8 +4,16 @@ using Microsoft.AspNetCore.Html;
 
 namespace Crews.Web.Cipher.Extensions;
 
+/// <summary>
+/// Extension methods for Cipher.
+/// </summary>
 public static class ElementExtensions
 {
+	/// <summary>
+	/// Converts an Element to an HtmlString for use in a web page.
+	/// </summary>
+	/// <param name="element">The element to convert.</param>
+	/// <returns>Returns a new HtmlString representing the Element.</returns>
 	public static HtmlString ToHtmlString(this Element element) =>
 		new HtmlString(element.ToString());
 }
